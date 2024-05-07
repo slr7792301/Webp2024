862d0eb91613eef127ada35c556262b42d5e6fb8
slr7792301 <59087577+slr7792301@users.noreply.github.com>
2024-05-07 19:23:23 +0800

    add

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SignIn from './signIn';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        { SignIn() }
+      </div>
+    </div>
+  );
+}
+
+export default App;
